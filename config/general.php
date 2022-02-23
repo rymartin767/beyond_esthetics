e330921140544c3f2138d587604f8213923b8cbd
Ryan Martin <ryan@nt4c.com>
2022-02-23 12:04:36 -0700

    added employees

---
+++ config/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'employees' => [
+        'Victor Ferrini' => [
+            'title' => 'Medical Director',
+            'subtitle' => 'Medical Director',
+            'image' => 'tonya.png',
+            'bio' => ''
+        ],
+        'Faith Holbrook' => [
+            'title' => 'Cosmetologist',
+            'subtitle' => 'Cosmetologist',
+            'image' => 'faith.png'
+        ],'Nicci See' => [
+            'title' => 'owner',
+            'subtitle' => 'Esthetician',
+            'image' => 'diane.png'
+        ],
+    ]
+];
