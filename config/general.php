7a4a902adc1682c5a10b9ca8d41e5816ba813c6a
Ryan Martin <ryan@nt4c.com>
2022-02-24 15:40:23 -0500

    wip

--- config/general.php
+++ config/general.php
@@ -1,21 +1,3 @@
 <?php
 
-return [
-    'employees' => [
-        'Victor Ferrini' => [
-            'title' => 'Medical Director',
-            'subtitle' => 'Medical Director',
-            'image' => 'tonya.png',
-            'bio' => ''
-        ],
-        'Faith Holbrook' => [
-            'title' => 'Cosmetologist',
-            'subtitle' => 'Cosmetologist',
-            'image' => 'faith.png'
-        ],'Nicci See' => [
-            'title' => 'owner',
-            'subtitle' => 'Esthetician',
-            'image' => 'diane.png'
-        ],
-    ]
-];
+//
