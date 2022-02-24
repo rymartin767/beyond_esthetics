442e60d51bf1d547750274e1cc977e3aa6825398
Ryan Martin <ryan@nt4c.com>
2022-02-24 15:41:06 -0500

    styling wip

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,7 +1,7 @@
-<div {{ $attributes->merge(['class' => 'max-w-7xl mx-auto pt-2 pb-6 px-4 sm:px-6 lg:px-8']) }}>
+<div {{ $attributes->merge(['class' => 'max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8']) }}>
     <div class="mb-6">
         @isset($title)
-            <div class="border-l-4 border-purple-400 pl-3 py-1">
+            <div class="border-l-4 border-spa-200 pl-3">
                 <div class="font-mont text-3xl sm:text-4xl">{{ $title }}</div>
             </div>
         @endisset
