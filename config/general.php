4d6aeebc188b0d1ff3cade4b765e602a8305d4b7
Ryan Martin <ryan@nt4c.com>
2022-02-25 20:08:28 -0500

    added hours

--- config/general.php
+++ config/general.php
@@ -1,3 +1,24 @@
 <?php
 
-//
+return [
+    'hours' => [
+        'ontario' => [
+            'Monday' => '7AM to 5PM',
+            'Tuesday' => '7AM to 5PM',
+            'Wednesday' => '7AM to 5PM',
+            'Thursday' => '7AM to 5PM',
+            'Friday' => '7AM to 5PM',
+            'Saturday' => '7AM to 5PM',
+            'Sunday' => '7AM to 5PM',
+        ],
+        'ashland' => [
+            'Monday' => '7AM to 5PM',
+            'Tuesday' => '7AM to 5PM',
+            'Wednesday' => '7AM to 5PM',
+            'Thursday' => '7AM to 5PM',
+            'Friday' => '7AM to 5PM',
+            'Saturday' => '7AM to 5PM',
+            'Sunday' => '7AM to 5PM',
+        ]
+    ]
+];
