0e8156fdee4413bc8b5760545e9023774eecd32d
Ryan Martin <ryan@nt4c.com>
2022-02-28 20:31:26 -0700

    nav links updated

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex sm:items-center">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('home')">
                         {{ __('HOME') }}
                     </x-jet-nav-link>
 
