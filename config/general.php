cb8e0780dd2a91b2d641e6afe0fb4fbbfc49102d
Ryan Martin <ryan@nt4c.com>
2022-03-03 16:31:01 -0700

    added dermaplane

--- config/general.php
+++ config/general.php
@@ -23,6 +23,19 @@ return [
     ],
 
     'logos' => [
+        'dermaplane' => [
+            'Mild Exfoliation' => '/images/services/logos/mild_exfoliation.svg',
+            'Pore Size' => '/images/services/logos/pore_size.svg',
+            'Semi Aggressive' => '/images/services/logos/semi_aggressive.svg',
+            'Fine Lines' => '/images/services/logos/fine_lines.svg',
+            'Brighten Skin' => '/images/services/logos/brighten_skin.svg',
+            'Fat Ablation' => '/images/services/logos/fat_ablation.svg',
+            'Collagen Inducing' => '/images/services/logos/collagen_inducing.svg',
+            'Cellular Turnover' => '/images/services/logos/cellular_turnover.svg',
+            'Tighten Skin' => '/images/services/logos/tighten_skin.svg',
+            'Acne Correction' => '/images/services/logos/acne_correction.svg',
+            'Stretch Marks and Scars' => '/images/services/logos/stretch_marks.svg'
+        ],
         'morpheus8' => [
             'Mild Exfoliation' => '/images/services/logos/mild_exfoliation.svg',
             'Pore Size' => '/images/services/logos/pore_size.svg',
