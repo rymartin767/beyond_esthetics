d5b12651d9fa5ceddc3cd129aa78d64507dc8bcd
Ryan Martin <ryan@nt4c.com>
2022-03-04 15:11:38 -0700

    updated for reviews

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -3,14 +3,18 @@
 namespace App\Http\Livewire;
 
 use App\Models\Employee;
+use App\Models\Review;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
+        $reviews = Review::where('name', 'General')->get();
+
         return view('livewire.home', [
-            'employees' => Employee::all()
+            'employees' => Employee::all(),
+            'reviews' => $reviews
         ]);
     }
 }
