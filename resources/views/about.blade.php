5a7a502d5259dd17a42fe00833f6fe6ae845ce57
Ryan Martin <ryan@nt4c.com>
2022-03-11 14:36:39 -0800

    MAR11 eve WIP

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,16 +2,50 @@
     <div class="bg-gray-100 h-96">
         TEAM PHOTO
     </div>
-    <div class="h-96 bg-gray-200">
-        MISSION STATEMENT
+    <div class="max-w-7xl mx-auto my-16">
+        <div class="text-center">
+            <div class="text-5xl text-blue-500 font-semibold mb-6">We assist. We empower. We inspire.</div>
+            <div class="flex flex-col space-y-6 text-2xl font-semibold leading-9">
+                <div>
+                    Fusion IV exists to infuse the world with healthy possibilities.</div>
+                <div>
+                    Our mission is to provide elevated wellness experiences through trusted and quality care. As disciples of health and wellness, compassion is in our DNA. We genuinely care about the wellbeing of each individual we serve, and we prove it by treating our customers like friends with personalized Assistance at every step of the way.
+                </div>
+                <div>
+                    As champions of confidence, achievement, and success, we’re here to Empower you to reach your full potential through a premium and professional IV therapy experience you won’t find anywhere else.
+                </div>
+                <div>
+                    And no matter your wellness goals, each of our hydrating, nutrient-rich, and vitamin-filled IV solutions are designed to Inspire you to move life forward and achieve remarkable outcomes
+                    along the way.
+                </div>
+                <div>
+                    We hope you enjoy your experience with Fusion IV, and that you stay a part of our family for
+                    years to come.
+                </div>
+                <div>
+                    Fusion IV Family
+                </div>
+            </div>
+        </div>
     </div>
     <div class="h-96 bg-gray-300">
-        OUR FOUNDER
+        next
     </div>
-    <div class="h-96 bg-gray-400">
-        <div x-data="{ shown: false }" x-intersect="shown = true">
-            <div x-show="shown" x-transition>
-                I'm in the viewport!
+    <div class="border-t-2 border-gray-400">
+        <div x-data="{ shown: false }" x-intersect="shown = true" class="max-w-6xl mx-auto py-24">
+            <div x-show="shown" x-transition.duration.5000ms class="grid grid-cols-3">
+                <div class="col-span-2">
+                    <div class="flex flex-col">
+                        <div class="text-4xl text-blue-500 font-semibold mb-4">Beyond Esthetics Founders</div>
+                        <div class="text-2xl font-bold mb-3">Tonya & Steve Goulding</div>
+                        <div class="text-lg font-semibold pr-8">Victoria and Eric are the husband-and-wife duo who are excited to welcome you to Fusion IV. Eric is a Scottsdale native, while Victoria grew up in the Bay Area. The two met in Coronado, California while Eric served in the Navy. Once Eric finished his service, the two moved back to Scottsdale where they started their family together. They have two daughters whom they hope to be role models for, especially in the health-and-wellness department, and they believe Fusion IV is a great steppingstone for achieving that. The Fitch’s enjoy spending time outside, going to the lake, and enjoying quality time with friends and family.</div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="">   
+                        <div class="h-96 w-96 rounded-full bg-slate-500"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
