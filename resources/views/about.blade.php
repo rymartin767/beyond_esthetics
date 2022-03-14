ed633fbc70700384f5dacf9222106152098d5492
Ryan Martin <ryan@nt4c.com>
2022-03-13 17:49:09 -0700

    MAR13 pm WIP

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,26 +1,32 @@
 <x-app-layout>
+
     <!-- TEAM PHOTO -->
-    <div class="bg-gray-100 min-h-screen sm:min-h-0 py-8 px-4 sm:px-0">
+    <x-section class="bg-pink-200">
         TEAM PHOTO
-    </div>
+    </x-section>
 
     <!-- MISSION STATEMENT -->
-    <div x-data="{ shown: false }" x-intersect="shown = true" class="min-h-screen sm:min-h-0 py-8 px-4 sm:px-0">
-        <div x-show="shown" x-transition.duration.4000ms class="max-w-7xl mx-auto">
-            <div class="text-5xl text-blue-500 font-semibold text-center">
-                <div class="flex flex-col space-y-2 sm:space-y-0 sm:space-x-2 sm:flex-row">
-                    <div>We Assist.</div>
-                    <div>We Empower.</div>
-                    <div>We Inspire.</div>
+    <x-section>
+        <div x-data="{ shown: false }" x-intersect="shown = true">
+            <div x-show="shown" x-transition.duration.4000ms class="max-w-7xl mx-auto">
+                <div class="text-5xl text-blue-500 font-semibold text-center">
+                    <div class="flex flex-col space-y-2 sm:space-y-0 sm:space-x-2 sm:flex-row">
+                        <div>We Assist.</div>
+                        <div>We Empower.</div>
+                        <div>We Inspire.</div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
+    </x-section>
+    
     <!-- FAQs -->
-    <div x-data="{ shown: false }" x-intersect="shown = true" class="min-h-screen sm:min-h-0 py-8 px-4 sm:px-0">
-        <div x-show="shown" x-transition.duration.5000ms>    
-            @include('layouts.elements.faq', compact('faqs'))
+    <x-section>
+        <div x-data="{ shown: false }" x-intersect="shown = true">
+            <div x-show="shown" x-transition.duration.5000ms>    
+                @include('layouts.elements.faq', compact('faqs'))
+            </div>
         </div>
-    </div>
+    </x-section>
+    
 </x-app-layout>
--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,13 +1,16 @@
-<div {{ $attributes->merge(['class' => 'max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8']) }}>
-    <div class="mb-6">
+<div {{ $attributes->merge(['class' => 'py-6 min-h-screen sm:min-h-full']) }}>
+    <div class="max-w-7xl mx-auto">
+        
         @isset($title)
-            <div class="border-l-4 border-spa-200 pl-3">
+            <div class="border-l-4 border-spa-200 pl-3 mb-6">
                 <div class="font-mont text-3xl sm:text-4xl">{{ $title }}</div>
             </div>
         @endisset
-    </div>
+        
 
-    <div>
-        {{ $slot }}
+        <div>
+            {{ $slot }}
+        </div>
+
     </div>
 </div>
