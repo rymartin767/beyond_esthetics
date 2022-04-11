4e7b5b567c7f94b34c318f76f39cc246c970c815
Ryan Martin <ryan@nt4c.com>
2022-04-11 11:05:07 -0400

    APR11 wip3

--- app/Filament/Resources/ServiceResource/Pages/EditService.php
+++ app/Filament/Resources/ServiceResource/Pages/EditService.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ServiceResource\Pages;
 
 use App\Filament\Resources\ServiceResource;
+use Filament\Pages\Actions\ButtonAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditService extends EditRecord
@@ -14,5 +15,21 @@ class EditService extends EditRecord
         $data['video_url'] = str($data['video_url'])->after('.be/');
     
         return $data;
+    }
+
+    protected function getFormActions(): array
+    {
+        return array_merge(parent::getFormActions(), [
+            ButtonAction::make('Clear Treatments')
+                ->action('clearServiceTreatments')
+                ->color('danger')
+        ]);
+    }
+ 
+    public function clearServiceTreatments(): void
+    {
+        $this->record->treats()->detach();
+        $this->record->treatments = [];
+        $this->record->save();
     }
 }
