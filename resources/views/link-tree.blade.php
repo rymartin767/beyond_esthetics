5d3b1f79597ec2154d5c83da2c1c9d32ce6b7275
ryanmartin767 <ryan@nt4c.com>
2022-08-12 10:38:49 -0700

    update links

--- resources/views/link-tree.blade.php
+++ resources/views/link-tree.blade.php
@@ -19,28 +19,43 @@
     <body>
         <div class="min-h-screen flex flex-col font-mont overflow-x-hidden">
             <main class="bg-spa-200 pt-12 flex-1">
-                <div class="max-w-sm mx-auto flex flex-col space-y-6">
+                <div class="max-w-sm mx-auto flex flex-col space-y-6 px-3">
                     <img src="/images/logo.svg" alt="">
                     <a href="/">
-                        <div class="cursor-pointer bg-black text-white rounded-full px-2 py-6 text-center">@beyondestheticsmedspa</div>
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">WEBSITE</div>
                     </a>
-                    <a href="https://www.facebook.com/BeyondEstheticsMansfield">
-                        <div class="cursor-pointer bg-spa-200 border border-white text-white rounded-full px-2 py-6 text-center">FACEBOOK</div>
+                    <a href="{{ route('ppt') }}">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">PRE/POST CARE TREATMENTS</div>
                     </a>
-                    <a href="https://www.instagram.com/beyondesthetics">
-                        <div class="cursor-pointer bg-spa-200 border border-white text-white rounded-full px-2 py-6 text-center">INSTAGRAM</div>
+                    <a href="mailto:beyondestheticsspa@gmail.com?subject = Consultation Request">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">SCHEDULE A CONSULTION</div>
                     </a>
-                    <a href="/specials">
-                        <div class="cursor-pointer bg-spa-200 border border-white text-white rounded-full px-2 py-6 text-center">SPECIALS</div>
+                    <a href="https://www.vagaro.com/beyondestheticsspa/book-now">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">SCHEDULE AN APPOINTMENT</div>
                     </a>
-                    <a href="/services">
-                        <div class="cursor-pointer bg-spa-200 border border-white text-white rounded-full px-2 py-6 text-center">SERVICES</div>
+                    <a href="https://alastin.com/BeyondEsthetics">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">SHOP ALASTIN</div>
                     </a>
-                    <a href="/about-us">
-                        <div class="cursor-pointer bg-spa-200 border border-white text-white rounded-full px-2 py-6 text-center">ABOUT US</div>
+                    <a href="{{ route('payment-plans') }}">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">PAYMENT PLANS</div>
                     </a>
-                    <a href="/payment-plans">
-                        <div class="cursor-pointer bg-spa-200 border border-white text-white rounded-full px-2 py-6 text-center">PAYMENT PLANS</div>
+                    <a href="https://www.instagram.com/beyondesthetics/?hl=en">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">INSTAGRAM</div>
+                    </a>
+                    <a href="https://www.facebook.com/BeyondEstheticsMansfield/">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">FACEBOOK</div>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCmSteVt8iblyoZyRRKhrFFw">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">YOUTUBE</div>
+                    </a>
+                    <a href="mailto:beyondestheticsspa@gmail.com?">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">EMAIL US</div>
+                    </a>
+                    <a href="https://beyondestheticsmedspa.com/training">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">PERMANENT MAKEUP TRAINING</div>
+                    </a>
+                    <a href="https://beyondestheticsmedspa.com/training">
+                        <div class="cursor-pointer bg-black border-2 border-white text-white rounded-full px-2 py-6 text-center">DERMAPLANE PRO TRAINING</div>
                     </a>
                 </div>
             </main>
